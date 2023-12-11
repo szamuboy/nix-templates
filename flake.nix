1feec96be8b3b8eb288a39b68b7b490a2547b554
Gergely Szabo <gergely79@gmail.com>
2023-12-11 11:44:14 +0100

    adding with-appuio-scripts template

--- flake.nix
+++ flake.nix
@@ -7,6 +7,10 @@
         description = "Development environment with scripts";
         path = ./with-scripts;
       };
+      with-appuio-scripts = {
+        description = "Development environment with scripts prepared for the APPuio environment";
+        path = ./with-scripts-appuio;
+      };
     };
 
   };
---
+++ with-scripts-appuio/scripts.nix
@@ -0,0 +1,31 @@
+{ writeShellApplication }:
+let
+  scripts = {
+    oc-login = {
+      description = "Login to Openshift";
+      runtimeInputs = [ openshift ];
+      command = ''
+      oc login --token="$APPUIO_TOKEN" \
+               --server=https://api.cloudscale-lpg-2.appuio.cloud:6443
+    '';
+    };
+  };
+  generated-scripts = builtins.map (name:
+    let value = scripts.${name};
+      in writeShellApplication {
+	inherit name;
+	runtimeInputs = value.runtimeInputs;
+	text = value.command;
+      }) (builtins.attrNames scripts);
+  help = writeShellApplication {
+    name = "help";
+    runtimeInputs = [ ];
+    text = ''
+      echo "This projects defines the following commands:"
+      echo
+      ${builtins.concatStringsSep "\n"
+	(builtins.map (name: ''echo "${name}" - ${scripts.${name}.description} '')
+          (builtins.attrNames scripts))}
+    '';
+  };
+  in generated-scripts ++ [ help ]
